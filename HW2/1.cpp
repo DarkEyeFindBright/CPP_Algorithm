cee3cc318d9df81dae75849aa22861ae88d30bdd
YUCHAO SU <su.yuchao@outlook.com>
2022-02-07 20:30:39 -0500

    Solved

--- HW2/1.cpp
+++ HW2/1.cpp
@@ -1,40 +1,31 @@
 #include<iostream>
+#include<random>
 using namespace std;
 
 
 void merge(int arr[], int p, int q, int r) {
-	if ((r - p) == 1) {
-		if (arr[p] > arr[r]) {
-			int temp = arr[p];
-			arr[p] = arr[r];
-			arr[r] = temp;
-		}
-		else
-			return;
-	}
+
+	if (arr[q] <= arr[q + 1])
+		return;
+	cout << "Loop" << "/n";
 	int n1 = q - p + 1;
 	int n2 = r - q;
 	int* L = new int[n1+1];
 	int* R = new int[n2+1];
 	for (int i = 0; i < n1; i++) {
-		L[i] = arr[i];
-		cout << L[i] << endl;
-
+		L[i] = arr[i+p];
 	}
 	for (int i = 0; i < n2; i++) {
-		R[i] = arr[i + q];
-		cout << R[i] << endl;
+		R[i] = arr[i + q+1];
 	}
 	L[n1] = INT_MAX;
 	R[n2] = INT_MAX;
 	int i = 0;
 	int j = 0;
 
-	for (int k = p; p <= r; k++) {
+	for (int k = p; k <= r; k++) {
 		if (L[i] <= R[j])
 		{
-			if (L[i] == INT_MAX)
-				continue;
 			arr[k] = L[i];
 			i++;
 		}
@@ -43,7 +34,12 @@ void merge(int arr[], int p, int q, int r) {
 			j++;
 		}
 	}
+
+	delete[] L;
+	delete[] R;
 }
+
+
 
 void mergeSort(int arr[], int p, int r) {
 	if (p >= r)
@@ -55,13 +51,28 @@ void mergeSort(int arr[], int p, int r) {
 
 
 int main() {
-	int a[5] = { 1,4,3,2,5 };
-	for(int i =0;i<5;i++)
-		cout << a[i] << " ";
+	random_device rd;  //Will be used to obtain a seed for the random number engine
+	mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
+	uniform_int_distribution<> dis(1, 100);//uniform distribution between 1 and 100
+	int n;
+	cout << "Enter the value of n "; // Entering Value of n
+	cin >> n;
 
-	mergeSort(a, 0, 4);
+	int* A=new int[n];
+
+	cout << "Randomly Generated Numbers are : " << '\n'; // Printing Randomly Generated Numbers
+	for (int i = 0; i < n; i++) {
+
+		A[i] = dis(gen);
+		cout << A[i] << '\t';
+	}
+	cout << "\n";
+	cout << "Sorted Numbers are : " << '\n'; // Printing Sorted Numbers
+	mergeSort(A, 0, n-1);
 	for (int i = 0; i < 5; i++)
-		cout << a[i] << " ";
+		cout << A[i] << "\t";
 	cout << endl;
 	return 0;
 }
+
+
